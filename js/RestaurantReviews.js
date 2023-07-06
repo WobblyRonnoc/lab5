3613a5c6bb52b34726703d8f29e0727680908706
Connor <WobblyProductions@hotmail.com>
2023-07-06 19:16:47 -0400

    most of Ajax 2

--- js/RestaurantReviews.js
+++ js/RestaurantReviews.js
@@ -2,25 +2,53 @@
 
 */
 
-//$.getScript("js/Config.js");
-
-// On Document Ready -> make ajax call
+// Fill restaurant selection DDL when document is ready
 $(function(){
-   $.ajax({
-      type: "GET",
-      url: urls["php"] + "?action=getRestaurants",
-      dataType: "json",
-      success: function(data){
-         $.each(data,function(index, value){
-            let option = $("<option></option>");
-            option.val(index)
-            option.text(value["0"]);
+    $.ajax({
+        type: "GET",
+        url: urls["php"].concat("?action=getRestaurants"),
+        dataType: "json",
+        success: function(data){
+            $.each(data,function(index, value){
+                let option = $("<option></option>");
+                option.val(index)
+                option.text(value["0"]);
 
-            $('#drpRestaurant').append(option);
-         });
-      },
-      error: function(event, request, settings){
-         window.alert('AjaxError' + ' : ' + settings);
-      }
-   });
+                $('#drpRestaurant').append(option);
+            });
+        },
+        error: function(event, request, settings){
+            window.alert('AjaxError' + ' : ' + settings);
+        }
+    });
+});
+
+$('#drpRestaurant').on("change", function(){
+    $.ajax({
+        type: "GET",
+        url: urls["php"].concat("?action=getRestaurantData&id=",$('#drpRestaurant').val()),
+        dataType: "json",
+        success: function(data){
+            //fill form fields with restaurant data using ID
+            $('#txtStreetAddress').val(data["streetAddress"]);
+            $('#txtCity').val(data["city"]);
+            $('#txtProvinceState').val(data["province"]);
+            $('#txtPostalZipCode').val(data["postalCode"]);
+            $('#txtSummary').val(data["summary"]);
+
+            //rating as a DDL containing range of numbers using min and max from JSON
+
+            //clear the DDL first some weird stuff is happening~!!
+            for (let i = data["ratingMin"]; i < data["ratingMax"]; i++) {
+                let option = $("<option></option>");
+                option.val(i)
+                option.text(i.toString());
+                $('#drpRating').append(option);
+            }
+
+        },
+        error: function(event, request, settings){
+            window.alert('AjaxError' + ' : ' + settings);
+        }
+    });
 });
--- php/RestaurantReviews.php
+++ php/RestaurantReviews.php
@@ -18,7 +18,39 @@ function GetRestaurantNames(SimpleXMLElement $restaurantData): array {
 $restaurantReviews = GetXmlObject();
 
 // on ready ajax response to fill restaurant select options
-if (isset($_GET["action"]) && $_GET["action"] == "getRestaurants"){
+if (isset($_GET["action"]) && $_GET["action"] == "getRestaurants") {
     $names = GetRestaurantNames($restaurantReviews);
     echo json_encode($names);
 }
+
+if (isset($_GET["action"]) && isset($_GET["id"]) && $_GET["action"] == "getRestaurantData") {
+
+
+    $id = (int)$_GET["id"];
+
+    $restaurant = $restaurantReviews->restaurants->restaurant[$id]; // Get the restaurant specific to the received ID
+
+    // convert XML data -> dictionary
+    $data = [];
+
+    //Address Data
+    $address = $restaurant->address;
+    $data["streetAddress"] = $address->streetNumber . " " . $address->streetName;
+    $data["city"] = (string)$address->city;
+    $data["province"] = (string)$address->province;
+    $data["postalCode"] = (string)$address->postalCode;
+
+    //Review Data
+    $review = $restaurant->review;
+    $data["summary"] = (string)$review->summary;
+    $data["rating"] = (int)$review->rating;
+    $data["ratingMin"] = (int)$review->rating['min'];
+    $data["ratingMax"] = (int)$review->rating['max'];
+
+    // respond with data as JSON string
+    echo json_encode($data);
+}
+
+
+
+
